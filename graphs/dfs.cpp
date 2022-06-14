15dc216d1d7ac6692e7e755f509bdbfc8ca711cb
Athul Sukumaran <athul.sukumaran@citrix.com>
2022-06-14 15:38:12 +0530

    Add comments

--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -18,22 +18,28 @@ class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
-        // Code here
+        //we use this stack to traverse the graph
         stack<int> s;
         bool visited[V] = {false};
+
+        //result vector
         vector<int> res;
         
+        //push the first element
         s.push(0);
         visited[0] = true;
          
         while(!s.empty())
         {
+            //get the element on top, pop it, add it to the result and find its neighbors in the next for loop
             int v = s.top();
             res.push_back(v);
             s.pop();
             
+            // traverse through all neighbors of v in the adjacency list
             for(int i=0; i<adj[v].size(); i++)
             {
+                //make sure the vertex is not traversed more than once
                 if(!visited[adj[v][i]])
                 {
                     s.push(adj[v][i]);
