576c9ef26f17790b23ff9a4735cb41d6819597de
Roshin Joju <roshinjojuc4141@gmail.com>
2022-10-20 13:44:36 +0530

    Update R_MinimumPathSum.java

--- dynamicProgramming/R_MinimumPathSum.java
+++ dynamicProgramming/R_MinimumPathSum.java
@@ -7,9 +7,9 @@
  * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
  * Note: You can only move either down or right at any point in time.
  * 
- * 1    3   1
- * 1    5   1
- * 4    2   1
+ * 1   3   1
+ * 1   5   1
+ * 4   2   1
  *
  * Example 1:
  * Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
